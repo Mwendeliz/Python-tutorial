d4c86cc99203a5924717901dc5c4c55c71765e67
Mwendeliz <mwendeliz9799@gmail.com>
2024-03-02 12:06:42 +0300

    My python tutorial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+location = input("Enter your Location: ")
+print(f"Hello {name}, you are {age} years old and you live in {location}")
